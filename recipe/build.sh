c3fb4430f035b294d7176b63be1b1a06918ddc9a
Ilya Lavrenov <ilya.lavrenov@intel.com>
2023-06-06 14:13:06 +0400

    Apply suggestions from code review
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 # !/usr/bin/env bash
 
-set +ex
+set -ex
 
-cmake ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release -S "$SRC_DIR" -B "$SRC_DIR/build" || exit 1
-cmake --build "$SRC_DIR/build" --config Release --parallel $CPU_COUNT --verbose || exit 1
+cmake ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release -S "$SRC_DIR" -B "$SRC_DIR/build"
+cmake --build "$SRC_DIR/build" --config Release --parallel $CPU_COUNT --verbose
 cmake --install "$SRC_DIR/build" --config Release --verbose
